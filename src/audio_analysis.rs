071bfdf606fd246ff3f18407718b653877a01c6e
Eshref Yozdemir <eshrefyoz@gmail.com>
2021-05-13 19:12:14 +0200

    Find peaks in smoothed signal and print top 5

--- src/audio_analysis.rs
+++ src/audio_analysis.rs
@@ -1,6 +1,7 @@
 use crate::note::Note;
 use rustfft::{num_complex::Complex, Fft, FftPlanner};
 use statrs::statistics::Median;
+use std::f64;
 use std::sync::Arc;
 
 pub struct AudioAnalyzer {
@@ -93,17 +94,54 @@ impl AudioAnalyzer {
 
 fn print_freq(freq_spectrum: &[f64], delta_f: f64, target_notes: &Vec<Note>) {
     let median = freq_spectrum.median();
-    let mut maxval = 0.0f64;
-    let mut maxidx = 0;
-    for i in 0..freq_spectrum.len() {
-        let currval = freq_spectrum[i];
-        if currval > maxval {
-            maxval = currval;
-            maxidx = i;
+    let mut peaks = find_peaks(freq_spectrum, Some(25.0 * median), Some(25));
+    peaks.sort_unstable_by(|a, b| b.value.partial_cmp(&a.value).unwrap());
+    let mut top_frequencies = [f64::NAN; 5];
+    for (i, p) in peaks.iter().enumerate() {
+        if i < 5 {
+            top_frequencies[i] = (p.idx as f64) * delta_f;
         }
     }
-    let max_freq = delta_f * (maxidx as f64);
-    println!("Median frequency {} Highest frequency {}", median, max_freq);
+    println!("Top 5 frequencies {:?}", &top_frequencies);
+}
+
+#[derive(Debug, PartialEq)]
+struct Peak<T> {
+    idx: usize,
+    value: T,
+}
+
+impl<T> Peak<T> {
+    fn new(idx: usize, value: T) -> Peak<T> {
+        Peak { idx, value }
+    }
+}
+
+fn find_peaks(
+    signal: &[f64],
+    min_height: Option<f64>,
+    min_peak_dist: Option<usize>,
+) -> Vec<Peak<f64>> {
+    let n_samples = signal.len();
+    if n_samples == 0 {
+        return Vec::new();
+    } else if n_samples == 1 {
+        return vec![Peak::new(0, signal[0])];
+    }
+    let min_height = min_height.unwrap_or(0.0);
+    let min_peak_dist = min_peak_dist.unwrap_or(0);
+    let mut out: Vec<Peak<f64>> = Vec::new();
+    for i in 0..n_samples {
+        let greater_than_left = i == 0 || (signal[i] > signal[i - 1] && signal[i] >= min_height);
+        let greater_than_right =
+            i == n_samples - 1 || (signal[i] > signal[i + 1] && signal[i] >= min_height);
+        if greater_than_left && greater_than_right && signal[i] >= min_height {
+            if out.is_empty() || i - out[out.len() - 1].idx >= min_peak_dist {
+                out.push(Peak::new(i, signal[i]));
+            }
+        }
+    }
+    out
 }
 
 fn moving_avg(signal: &mut [f64], window_size: usize) {
@@ -134,8 +172,8 @@ fn moving_avg(signal: &mut [f64], window_size: usize) {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod tests_moving_avg {
+    use super::moving_avg;
 
     #[test]
     fn moving_avg_empty_arr() {
@@ -199,5 +237,49 @@ mod tests {
         let expected = vec![3.0, 3.0];
         moving_avg(&mut signal, 4);
         assert_eq!(expected, signal);
+    }
+}
+
+#[cfg(test)]
+mod tests_find_peaks {
+    use super::{find_peaks, Peak};
+
+    #[test]
+    fn find_peaks_empty_arr() {
+        let signal = Vec::new();
+        let out = find_peaks(&signal, None, None);
+        assert_eq!(out.len(), 0);
+    }
+
+    #[test]
+    fn find_peaks_single_elem() {
+        let signal = vec![1.0];
+        let expected = vec![Peak::new(0, 1.0)];
+        let actual = find_peaks(&signal, None, None);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn find_peaks_decreasing() {
+        let signal = vec![1.0, 0.5, 0.25];
+        let expected = vec![Peak::new(0, 1.0)];
+        let actual = find_peaks(&signal, None, None);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn find_peaks_parabola() {
+        let signal = vec![1.0, 0.5, 0.25, 0.5, 1.0];
+        let expected = vec![Peak::new(0, 1.0), Peak::new(4, 1.0)];
+        let actual = find_peaks(&signal, None, None);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn find_peaks_two_peaks() {
+        let signal = vec![0.5, 1.0, 2.0, 1.0, 0.0, 5.0, 2.5];
+        let expected = vec![Peak::new(2, 2.0), Peak::new(5, 5.0)];
+        let actual = find_peaks(&signal, None, None);
+        assert_eq!(expected, actual);
     }
 }
