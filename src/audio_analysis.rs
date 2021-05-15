90d654a3d7f36b539702d1a02aae77d75ac92dea
Eshref Yozdemir <eshrefyoz@gmail.com>
2021-05-15 20:35:14 +0200

    Create channel between audio and UI threads
    * AnalysisResult struct is converted to a FrameData and sent
    to the UI thread through the channel. Currently the spectrogram
    is sent as a copy.
    * Plot the spectrogram on the UI thread.

--- src/audio_analysis.rs
+++ src/audio_analysis.rs
@@ -1,5 +1,7 @@
 mod algorithm;
+mod analysis_result;
 mod analyzer;
 mod target_notes;
 
+pub use analysis_result::AnalysisResult;
 pub use analyzer::AudioAnalyzer;
---
+++ src/visualization.rs
@@ -0,0 +1,5 @@
+mod frame_data;
+mod visualizer;
+
+pub use frame_data::FrameData;
+pub use visualizer::Visualizer;
